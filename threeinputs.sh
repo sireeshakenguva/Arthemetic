954961f6887863d018726551d264893555105df6
sireeshakenguva <sireeshakenguva@gmail.com>
2022-04-05 06:52:03 -0700

    [sireeshakenguva]ADDED implemented storeresult problem in Arthemetic problem

--- threeinputs.sh
+++ threeinputs.sh
@@ -10,3 +10,9 @@ z=$(($c+$a/$b))
 echo "value of compute3 is:"$z
 p=$(($a%$b+$c))
 echo "value of compute4 is:"$p
+declare -a Result
+Result[0]=$x
+Result[1]=$y
+Result[2]=$z
+Result[3]=$p
+echo "The Results of every Computation in Dictionary :" ${Result[@]}
